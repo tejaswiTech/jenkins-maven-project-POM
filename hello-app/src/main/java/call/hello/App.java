8c168a3b41d9af815e5476d7e3cf01a72d11d1c7
tejaswi.rajput <tejaswi.rajput@del1-lhp-n69674.synapse.com>
2024-05-09 14:39:49 +0530

    commited changes 1.1

--- hello-app/src/main/java/call/hello/App.java
+++ hello-app/src/main/java/call/hello/App.java
@@ -9,6 +9,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("This is cloned project that taken from Github to gitv and now edited in Eclipse then pushed back to github");
     }
 
     private final String getMessage() {
